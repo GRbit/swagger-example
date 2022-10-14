7c6a228daed02f16be0288b6f6410d666d3ccaad
Pavel @grbit Griaznov <grbitt@gmail.com>
2022-10-14 09:52:52 +0200

    java server handlers

--- go.mod
+++ go.mod
@@ -5,5 +5,5 @@ go 1.19
 require (
 	github.com/goccy/go-json v0.9.11
 	github.com/gorilla/mux v1.8.0
-	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1
 )
--- internal/java/handlers/handlers.go
+++ internal/java/handlers/handlers.go
@@ -6,17 +6,45 @@ import (
 	"github.com/grbit/swagger-example/pkg/java/models"
 )
 
+var DB map[string]models.PropertyInfoData
+
 func HandleCreateUpdatePropertyInfoById(r *http.Request, body models.PropertyInfoData, propertyID string,
 ) (
 	models.PropertyInfoResponse, error,
 ) {
+	if DB == nil {
+		DB = make(map[string]models.PropertyInfoData)
+	}
+
+	DB[propertyID] = body
+
 	return models.PropertyInfoResponse{}, nil
 }
 
 func HandleGetPropertyInfoById(r *http.Request, propertyID string) (models.PropertyInfoResponse, error) {
-	return models.PropertyInfoResponse{}, nil
+	p, ok := DB[propertyID]
+	if !ok {
+		return models.PropertyInfoResponse{
+			Data:      &models.PropertyInfoData{},
+			RequestId: "",
+		}, nil
+	}
+
+	return models.PropertyInfoResponse{
+		Data:      &p,
+		RequestId: "",
+	}, nil
 }
 
 func HandleGetPropertiesInfo(r *http.Request) (models.PropertiesInfoResponse, error) {
-	return models.PropertiesInfoResponse{}, nil
+	pp := []models.PropertyInfoData{}
+
+	for _, p := range DB {
+		pp = append(pp, p)
+	}
+
+	return models.PropertiesInfoResponse{
+		Data:      pp,
+		RequestId: "",
+	}, nil
 }
